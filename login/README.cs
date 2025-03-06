9c31eb69beb3de71845df5fe60cfa33446be1064
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-03-06 10:49:00 -0600

    'readme'

--- login/README.cs
+++ login/README.cs
@@ -1,16 +1,16 @@
-﻿PASOS PARA LEVANTAR PROYECTO
---------------------------------
+﻿//PASOS PARA LEVANTAR PROYECTO
+//--------------------------------
 
-1-Revisar que las dependencias siguientes esten en el nuge packet manager:
+//1-Revisar que las dependencias siguientes esten en el nuge packet manager:
 
-- Microsoft.EntityFrameWorkCore
-- Microsoft.EntityFrameWorkCore.SqlServer
-- Microsoft.EntityFrameWorkCore.Tools
+//- Microsoft.EntityFrameWorkCore
+//- Microsoft.EntityFrameWorkCore.SqlServer
+//- Microsoft.EntityFrameWorkCore.Tools
 
-2-En Herramientas>Administrador de paquetes NuGet>Consola de administrador de paquetes, ejecutar el comando:
-```Add-Migration "primera migracion"
-```Update-Database
+//2-En Herramientas>Administrador de paquetes NuGet>Consola de administrador de paquetes, ejecutar el comando:
+//```Add-Migration "primera migracion"
+//```Update-Database
 
-3-Ejecutar proyecto
+//3-Ejecutar proyecto
 
-4-ir a ruta [url generada]/items
+//4-ir a ruta [url generada]/items
